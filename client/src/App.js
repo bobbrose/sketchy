f97ebe2812a2d317b6b45df253109ca605db70c4
Bob Rose <bobbrose@gmail.com>
2025-01-02 20:13:37 -0700

    Analyics debugging

--- client/src/App.js
+++ client/src/App.js
@@ -151,8 +151,6 @@ function App() {
             </div>
           )}
           {!image && !loading && !error && <p>Your generated image will appear here</p>}
-          <Analytics />
-
         </div>
       </div>
       {toast && <div className="toast">{toast}</div>}
